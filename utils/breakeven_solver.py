a90cd2d8c0dae32564ad2c0d41b38e6779f8930d
aye5788 <166234294+aye5788@users.noreply.github.com>
2025-11-23 14:35:30 -0500

    Update breakeven_solver.py

--- utils/breakeven_solver.py
+++ utils/breakeven_solver.py
@@ -1,14 +1,11 @@
 import numpy as np
 
-def breakevens(prices, long_vals, short_vals):
-    # total PL = long - short - debit
-    pl = long_vals - short_vals
-
-    # sign changes = breakevens
+def breakevens(prices, long_vals, short_vals, debit):
+    pl = long_vals - short_vals - debit
     signs = np.sign(pl)
     idx = np.where(np.diff(signs))[0]
 
     if len(idx) < 2:
         return None, None
 
-    return prices[idx[0]], prices[idx[1]]
+    return prices[idx[0]], prices[idx[-1]]
